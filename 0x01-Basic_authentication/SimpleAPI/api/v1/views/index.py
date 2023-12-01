74219ba6c0f231778f67b778ce2068eb323dfc98
Kehinde Opaleye <kenny@LENOVO>
2023-12-01 17:33:32 +0100

    1. Error handler: Unauthorized

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -1,30 +1,32 @@
-#!/usr/bin/env python3
-""" Module of Index views
+"""
+Module of Index views
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
 
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
+    """GET /api/v1/status
+    Returns:
+        - the status of the API
     """
     return jsonify({"status": "OK"})
 
 @app_views.route("/unauthorized", methods=["GET"])
 def unauthorized():
+    """GET /api/v1/unauthorized
+    Raises:
+        - 401 error to test unauthorized error handler
+    """
     abort(401)
 
-
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
+    """GET /api/v1/stats
+    Returns:
+        - the number of each object
     """
     from models.user import User
-    stats = {}
-    stats['users'] = User.count()
+    stats = {"users": User.count()}
     return jsonify(stats)
+
